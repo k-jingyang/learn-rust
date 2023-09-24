94ee18e4cc0e214087e3016771b49eb6702092e6
Jing Yang <k.jingyang@gmail.com>
2023-09-25 00:23:03 +0800

    fix day2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,23 +1,27 @@
 use std::fs;
 
 fn main() {
-
     let contents = fs::read_to_string("./input.txt").expect("Should have been able to read file");
     let lines = contents.split("\n");
 
-    for line in lines  {
+    let mut elf_calorie = 0;
 
-        
+    let mut elf_calories: Vec<i32> = Vec::new();
+
+    for line in lines {
         if line != "" {
+            let calories = line.trim().parse::<i32>().unwrap();
+            elf_calorie += calories;
         } else {
+            elf_calories.push(elf_calorie);
+            elf_calorie = 0
         }
     }
 
-}
+    elf_calories.sort();
 
-fn calculate_score(line: &str) -> i32 {
-    let fight = line.split(" ").collect();
-    fight[0];
-    fight[1];
-    return 1
+    let sum = elf_calories[elf_calories.len() - 3]
+        + elf_calories[elf_calories.len() - 2]
+        + elf_calories[elf_calories.len() - 1];
+    println!("{}", sum)
 }
