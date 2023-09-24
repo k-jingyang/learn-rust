a395b975cab5019d0e576732ccd20cba162391fd
Jing Yang <k.jingyang@gmail.com>
2023-09-25 00:23:15 +0800

    add day3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+# Learning Rust using Advent of Code 2022
+
+## Start a new day/project
+
 cargo new --vcs=none my-new-project
--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,29 +1,49 @@
 use std::fs;
 
 fn main() {
-
     let contents = fs::read_to_string("./input.txt").expect("Should have been able to read file");
     let lines = contents.split("\n");
-
-    let mut elf_calorie = 0;
-
-    let mut elf_calories : Vec<i32> = Vec::new();
-
-    for line in lines  {
-
-        if line != "" {
-            let calories = line.trim().parse::<i32>().unwrap();
-            elf_calorie += calories;
-        } else {
-            elf_calories.push(elf_calorie);
-            elf_calorie = 0
-        }
+    let mut score = 0;
+    for line in lines {
+        let splitted: Vec<&str> = line.split(" ").collect();
+        let match_score = get_score(splitted[0], splitted[1]);
+        score += match_score
     }
-
-    elf_calories.sort();
-
-    let sum = elf_calories[elf_calories.len()-3] + elf_calories[elf_calories.len()-2] +elf_calories[elf_calories.len()-1];
-    println!("{}", sum)
-
+    println!("{}", score)
 }
 
+fn get_score(opponent: &str, mine: &str) -> i32 {
+    match opponent {
+        "A" => match mine {
+            "X" => 1 + 3,
+            "Y" => 2 + 6,
+            "Z" => 3 + 0,
+            _ => {
+                println!("Unexpected value");
+                0
+            }
+        },
+        "B" => match mine {
+            "X" => 1 + 0,
+            "Y" => 2 + 3,
+            "Z" => 3 + 6,
+            _ => {
+                println!("Unexpected value");
+                0
+            }
+        },
+        "C" => match mine {
+            "X" => 1 + 6,
+            "Y" => 2 + 0,
+            "Z" => 3 + 3,
+            _ => {
+                println!("Unexpected value");
+                0
+            }
+        },
+        _ => {
+            println!("Unexpected value");
+            0
+        }
+    }
+}
